d7815c6357c4f29380fc9ef3b3ee93ba393410c7
Sebastian Quesada <SebastianQuesadaMarin@gmail.com>
2021-06-08 19:33:06 -0500

    Final Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AssetList from './components/asset-list/asset-list';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cryptoteros</h1>
+        <AssetList></AssetList>
+      </header>
+    </div>
+  );
+}
+
+export default App;
